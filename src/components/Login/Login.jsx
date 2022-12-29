f3c57ec65fb3a8b7dd239731c321b29770d0f938
Kiran Gangurde <89308541+kiran3311@users.noreply.github.com>
2022-12-29 16:04:34 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -58,7 +58,7 @@ const Login = () => {
     </div>
 
     <div>
-      <div className='table'>
+      <div className='table' style={{"color": "white"}}>
       <Table  bordered >
      
       <tbody>
